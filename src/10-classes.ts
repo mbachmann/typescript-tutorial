48cebe0daf996136b59fed468b3b4b5f080ac261
matthias.bachmann <bacn@zhaw.ch>
2017-03-17 21:39:01 +0100

    Update classes.ts

--- src/10-classes.ts
+++ src/10-classes.ts
@@ -11,7 +11,50 @@ export function classes () {
 
     // --------------------------------------
     // Classes
+    // Class can have a constructor, attributes and methods.
+
+    class Person {
+        bornOn: Date;
+
+        constructor(public name: string) {
+            this.bornOn = new Date();
+        }
+
+        shout(): void {}
+    }
+
+    // --------------------------------------
+    // Classes
+    // Class attributes and methods can be public or private.
+
+    class Person2 {
+        bornOn: Date; // public by default
+        public name: string;
+        private weight: number;
+    }
 
 
+    // Create new instances with the new keyword.
+    const john = new Person('John');
+
+    console.log('John ' + john.bornOn); // => a Date object
+    john.shout(); // => nothing but alerts
+
+
+    // --------------------------------------
+    // Classes
+    // Inheritance
+
+    class Employee extends Person {
+         constructor(name: string, public salary: number) {
+            super(name);
+             // ...
+        }
+    }
+
+    // Create new instances with the new keyword.
+    const felix = new Employee('Felix',10000);
+
+    console.log('Felix ' + felix.bornOn + ', '+ felix.salary); // => a Date object
 
 }
